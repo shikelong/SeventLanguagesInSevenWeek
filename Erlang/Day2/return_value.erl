07e9e21e3da52859340d2b713770854f19b31c56
shikelong <578622705@qq.com>
2021-12-13 23:28:38 +0800

    temp: upload erlang day2's homework

--- Erlang/Day2/return_value.erl
+++ Erlang/Day2/return_value.erl
@@ -1,8 +1,7 @@
 % 考虑包含键－值元组的列表，如[{erlang, "a functional language"}, {ruby, "an OO language"}]。
 % 写一个函数，接受列表和键为参数，返回该键对应的值。
+-module(return_value).
+-export([getByKey/2]).
 
-FilterByKey = fun(X) -> element(1, X) == ruby end.
-
-get_value(List, Key) ->
-  lists:filter(FilterByKey, List)
+getByKey(K, L) -> [element(2, I) || I <- L, element(1, I) == K].
 
--- Erlang/Day2/tic_tac_toe.erl
+++ Erlang/Day2/tic_tac_toe.erl
@@ -1,3 +1,13 @@
 % ：读取一个大小为9的列表或元组，表示井字棋（tic-tac-toe）的棋盘。若胜负已定，
 % 则返回胜者（x或o）；若再没有可走的棋着，则返回cat；若两方都还没赢，则返回
 % no_winner。
+-module(tic_tac_toe).
+-export([read/1]).
+
+
+% Board = [_,x,_,o,_,_o,x,_].
+
+% IsResolved = fun(X) -> member(X, [o, x]) end.
+
+% read(Board) -> lists:all(, Board).
+    
